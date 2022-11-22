ed9e2099fd8d926f3a0e4aff3fad7232b5c00231
sabiq mohammed <sabiq.mohammed4@gmail.com>
2022-11-22 07:50:12 +0530

    aligned ui added button app wrapped up

--- bq_app/lib/main.dart
+++ bq_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,8 +14,10 @@ class BinaryQuestion extends StatefulWidget {
 }
 
 class _BinaryQuestionState extends State<BinaryQuestion> {
+  int image = 2;
   @override
   Widget build(BuildContext context) {
+    image = Random().nextInt(2) + 1;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -23,9 +27,30 @@ class _BinaryQuestionState extends State<BinaryQuestion> {
             child: Text("B I N A R Y  ?'S"),
           ),
         ),
-        body: Center(
-          child: Image.asset(
-            'images/yes1.png',
+        body: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Text(
+                'Ask Your Yes or No Questions',
+                style: TextStyle(fontSize: 25.0),
+              ),
+              Divider(
+                color: Colors.black,
+                height: 40.0,
+              ),
+              SizedBox(
+                height: 50.0,
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    print('$image');
+                  });
+                },
+                child: Image.asset('images/$image.png'),
+              ),
+            ],
           ),
         ),
       ),
